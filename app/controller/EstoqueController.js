6611ab70b29626381faa1f495908c79ce3f93953
felipe guimarães <feliphe133bolado@gmail.com>
2024-12-27 16:56:05 -0300

    first commit

---
+++ app/controller/EstoqueController.js
@@ -0,0 +1,7 @@
+const Produto = require("../model/Produto");
+ 
+const novoProduto =  new Produto("Nissin Miojo", 15, 4);
+
+ console.log(novoProduto.getNome());
+ 
+
---
+++ app/model/Produto.js
@@ -0,0 +1,22 @@
+class Produto{
+
+    constructor(nome,quantidade,valor){
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    };
+    
+    getNome(){
+        return this.nome;   
+    }
+
+    getQuantidade(){
+        return this.quantidade;
+    }
+
+    getValor(){
+        return this.valor;
+    }
+
+    module.exports = Produto;
+}
